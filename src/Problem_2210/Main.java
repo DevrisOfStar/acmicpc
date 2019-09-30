c10220f50b01c7d735cfd77d6151992cad8fcbc8
Nokda <yh1483@naver.com>
2019-09-30 15:38:48 +0900

    2019-09-30 Problem 2210 Solved.

---
+++ src/Problem_2210/Main.java
@@ -0,0 +1,46 @@
+package Problem_2210;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+	static int[] dx = {0,0,-1,1};
+	static int[] dy = {-1,1,0,0};
+	static int[][] arr = new int[5][5];
+	static Set<Integer> ans = new HashSet<>();
+	public static void func(int i, int j, int cnt, int data) {
+		if(cnt == 6) {
+			ans.add(data);
+		}
+		else {
+			for(int k = 0 ; k < 4; k++) {
+				int dx_ = i + dx[k];
+				int dy_ = j + dy[k];
+				if(0> dx_ || 0> dy_ || dx_ >=5 || dy_ >=5) continue;
+				func(dx_, dy_, cnt+1, data*10+arr[dx_][dy_]);
+			}
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str;
+		for(int i = 0; i < 5; i++) {
+			str = br.readLine();
+			for(int j = 0 ; j < 5; j++) {
+				arr[i][j] = str.charAt(j*2) -'0';
+			}
+		}
+		
+		for(int i = 0 ; i < 5; i++) {
+			for(int j = 0 ; j < 5; j++) {
+				func(i,j, 1,arr[i][j]);
+			}
+		}
+		System.out.println(ans.size());
+		
+		
+	}
+}
