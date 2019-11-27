635afe9ada73970d55241a7778419426df28b328
Nokda <yh1483@naver.com>
2019-11-28 00:02:41 +0900

    2019-11-28 Problem 14395 Solved.

---
+++ src/Problem_14395/Main.java
@@ -0,0 +1,53 @@
+package Problem_14395;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Data {
+	long number;
+	String ans;
+	public Data(long number, String ans) {
+		this.number = number;
+		this.ans = ans;
+	}
+	
+}
+public class Main {
+	static String d[] = {"*","+","-","/"};
+	public static long Calculate(long number, int i) {
+		if(i == 0) return number * number;
+		else if(i == 1) return number + number;
+		else if(i == 2) return number - number;
+		else return number / number;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long s = sc.nextLong();
+		long t = sc.nextLong();
+		if(s == t) {System.out.println("0"); return;}
+		ArrayList<Long> arr = new ArrayList<>();
+		Queue<Data> q = new LinkedList<>();
+		q.add(new Data(s, ""));
+		arr.add(s);
+		while(!q.isEmpty()) {
+			Data data = q.poll();
+			String st = data.ans;
+			
+			if(data.number == t) {
+				System.out.println(st);
+				return;
+			}
+			for(int i = 0; i < 4; i++) {
+				long dn = Calculate(data.number, i);
+				if (dn < 1) continue;
+				if(arr.contains(dn)) continue;
+				arr.add(dn);
+				q.add(new Data(dn, st+d[i]));
+			}
+		}
+		System.out.println(-1);
+	}
+}
