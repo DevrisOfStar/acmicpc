567b96371c50d36542ef1cf9f8adc2265ce6fd64
Nokda <yh1483@naver.com>
2020-02-18 21:14:36 +0900

    2020-02-18 Problem 2089 Solved.

---
+++ src/Problem_2089/Main.java
@@ -0,0 +1,25 @@
+package Problem_2089;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		if (N == 0) {
+			System.out.println("0");
+			return;
+		}
+		while (N != 1) {
+			sb.append(Math.abs(N % (-2)));
+			N = (int) Math.ceil((double) N / (-2));
+		}
+		sb.append(N);
+
+		System.out.print(sb.reverse().toString());
+
+	}
+}
