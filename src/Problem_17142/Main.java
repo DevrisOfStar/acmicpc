bddce116639e98ac61de88fab481785d93cf0c28
Nokda <yh1483@naver.com>
2020-03-25 15:10:19 +0900

    2020-03-25 Problem 17142 Solved.

--- src/Problem_17142/Main.java
+++ src/Problem_17142/Main.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -22,20 +21,20 @@ public class Main {
 	public static void bfs(int idx, int number) {
 		if (idx == M) {
 			Queue<int[]> q = new LinkedList<>();
-			Queue<int[]> q2 = new LinkedList<>();
-			System.out.println(Arrays.toString(virus_arr));
-
+			Queue<int[]> history = new LinkedList<>();
 			for (int i = 0; i < virus_arr.length; i++) {
 				arr[virus.get(virus_arr[i])[0]][virus.get(virus_arr[i])[1]] = 2;
-				q.add(new int[] { virus.get(virus_arr[i])[0], virus.get(virus_arr[i])[1], 0 });
+				q.add(new int[] { virus.get(virus_arr[i])[0], virus.get(virus_arr[i])[1]});
 				isvisited[virus.get(virus_arr[i])[0]][virus.get(virus_arr[i])[1]] = true;
 			}
 			int cnt = 0;
-			for (; !q.isEmpty(); cnt++) {
-				int j = q.size();
-				for(; !q.isEmpty();) {
+			int temp = 0;
+
+			for (; !q.isEmpty();) {
+				boolean blank = false;
+				int size = q.size();
+				for (int k = 0; k < size; k++) {
 					int[] data = q.poll();
-					
 					for (int i = 0; i < 4; i++) {
 						int dx_ = data[0] + dx[i];
 						int dy_ = data[1] + dy[i];
@@ -46,29 +45,22 @@ public class Main {
 							continue;
 						if (arr[dx_][dy_] == -1 && !isvisited[dx_][dy_]) {
 							isvisited[dx_][dy_] = true;
-							q.add(new int[] { dx_, dy_});
+							q.add(new int[] {dx_, dy_});
 						} else if (arr[dx_][dy_] == 0 && !isvisited[dx_][dy_]) {
 							isvisited[dx_][dy_] = true;
-							q2.add(new int[] { dx_, dy_});
+							blank = true;
+							q.add(new int[] { dx_, dy_});
 						}
 					}
 				}
-				for(;!q2.isEmpty();) q.add(q2.poll());
-				
-				if(!Arrays.toString(virus_arr).equals("[0, 1, 2]")) continue;
-				System.out.println(cnt);
-				for (int i = 0; i < N; i++) {
-					for (int k = 0; k < N; k++) {
-						if ((arr[i][k] == 0 || arr[i][k] == 2 || arr[i][k] == -1) && isvisited[i][k])
-							System.out.print("1 ");
-						else
-							System.out.print("0 ");
-					}
-					System.out.println();
-				}	
-				System.out.println();
+				if(!blank) {
+					temp++;
+				}else {
+					cnt += ++temp;
+					temp = 0;
+				}
 			}
-			
+
 			boolean flag = true;
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
@@ -81,32 +73,24 @@ public class Main {
 
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					if ((arr[i][j] == 0 || arr[i][j] == 2 || arr[i][j] == -1) && isvisited[i][j])
-						System.out.print("1 ");
-					else
-						System.out.print("0 ");
-				}
-				System.out.println();
-			}
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
 					isvisited[i][j] = false;
 				}
 			}
 			for (int i = 0; i < virus_arr.length; i++) {
 				arr[virus.get(virus_arr[i])[0]][virus.get(virus_arr[i])[1]] = -1;
 			}
+
 			if (flag) {
-				System.out.println(cnt);
 				if (min > cnt)
 					min = cnt;
 			}
 
-			System.out.println();
 
 		} else if (number == virus.size())
 			return;
-		else {
+		else
+
+		{
 			virus_arr[idx] = number;
 			bfs(idx + 1, number + 1);
 			bfs(idx, number + 1);
@@ -135,6 +119,7 @@ public class Main {
 			}
 		}
 		bfs(0, 0);
-		System.out.println(min);
+		if(min == Integer.MAX_VALUE) System.out.println("-1");
+		else System.out.println(min);
 	}
 }
