51efd74d3b7665f887307d743dcc88313380a4dc
Nokda <yh1483@naver.com>
2020-03-30 18:29:42 +0900

    2020-03-30 Problem 11509 Solved.

---
+++ src/Problem_11509/Main.java
@@ -0,0 +1,30 @@
+package Problem_11509;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		int cnt = 0;
+		int[] arrow = new int[1000001];
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			if (arrow[arr[i] + 1] == 0) {
+				arrow[arr[i]]++;
+				cnt++;
+			} else {
+				arrow[arr[i] + 1]--;
+				arrow[arr[i]]++;
+			}
+		}
+
+		System.out.println(cnt);
+
+	}
+}
