1ca6c20dfd14d2bac03f5a2e5b9de84456a3ff47
Nokda <yh1483@naver.com>
2020-03-31 02:35:53 +0900

    2020-03-31 Problem 10951 Solved.

---
+++ src/Problem_10951/Main.java
@@ -0,0 +1,16 @@
+package Problem_10951;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = 0, b = 0;
+		do {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			System.out.println(a + b);
+		} while (sc.hasNextInt());
+	}
+}
