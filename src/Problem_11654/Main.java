792707eb0d6745110b918c4f0980f809c1d3840a
Nokda <yh1483@naver.com>
2020-03-31 16:34:11 +0900

    2020-03-31 Problem 11654 Solved.

---
+++ src/Problem_11654/Main.java
@@ -0,0 +1,11 @@
+package Problem_11654;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String n = sc.next();
+		System.out.println((int)(n.charAt(0)));
+	}
+}
