02c58410efa3ba43b4a2282c2be7cfa176b2381b
Nokda <yh1483@naver.com>
2020-04-02 22:20:54 +0900

    2020-04-02 Problem 10171 Solved.

---
+++ src/Problem_10171/Main.java
@@ -0,0 +1,10 @@
+package Problem_10171;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\\n" + 
+				" )  ( ')\n" + 
+				"(  /  )\n" + 
+				" \\(__)|");
+	}
+}
