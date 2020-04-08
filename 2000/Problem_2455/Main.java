33dba20900f21b57084dac420b4c4505d0aa93e8
Nokda <yh1483@naver.com>
2020-04-08 15:19:50 +0900

    2020-04-08 Problem 2455 reviewed and added comment.

--- 2000/Problem_2455/Main.java
+++ 2000/Problem_2455/Main.java
@@ -12,12 +12,13 @@ public class Main {
 		int a, b;
 		int in_train = 0;
 		int max = 0;
-		for(int i = 0 ; i < 4; i++) {
+		for(int i = 0 ; i < 4; i++) { // 역은 4개
 			st = new StringTokenizer(br.readLine()," ");
 			a = Integer.parseInt(st.nextToken());
 			b = Integer.parseInt(st.nextToken());
 			in_train += b;
 			in_train -= a;
+			// 정원은 10000명이나 초과하여 타는 경우는 없으므로, max 계산을 간단하게 할 수 있다.
 			if(max < in_train) max = in_train;
 		}
 		
