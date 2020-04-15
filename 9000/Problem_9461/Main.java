858620d85445a90b58c8671f6bdee95f71596287
Nokda <yh1483@naver.com>
2020-04-15 17:37:34 +0900

    2020-04-15 Problem 9461 added Comment and reviewed.

--- 9000/Problem_9461/Main.java
+++ 9000/Problem_9461/Main.java
@@ -10,7 +10,8 @@ public class Main {
 		
 		arr[1] = 1;
 		arr[2] = 1;
-		
+		// 1, 1, 1, 2, 2, 3, 4, 5, 7, 9
+		// 규칙 --> A_i = A_(i-2) + A_(i-3)
 		for(int i = 3; i<=100;i++) {
 			arr[i] = arr[i-2] + arr[i-3];
 		}
