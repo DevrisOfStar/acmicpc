171199fa1d5a1ea3a1992b43f4bc3b9279758d70
Nokda <yh1483@naver.com>
2020-04-15 17:33:04 +0900

    2020-04-15 Problem 3568 added Comment and reviewed.

--- 3000/Problem_3568/Main.java
+++ 3000/Problem_3568/Main.java
@@ -11,13 +11,13 @@ public class Main {
 		String str = br.readLine();
 		
 		StringTokenizer st = new StringTokenizer(str, ", ");
-		String common = st.nextToken();
+		String common = st.nextToken(); // 공통 변수형
 		String temp;
 		StringBuilder sb;
 		StringBuilder sb2;
 		do {
-			sb = new StringBuilder();
-			sb2 = new StringBuilder();
+			sb = new StringBuilder(); // 개별 변수형
+			sb2 = new StringBuilder(); // 개별 변수명
 			temp = st.nextToken();
 			for(int i = 0; i<temp.length();i++) {
 				if(temp.charAt(i) == '[') sb.append(']');
@@ -28,6 +28,6 @@ public class Main {
 				else sb.append(temp.charAt(i));
 			}
 			System.out.println(common + sb.reverse().toString() + " " + sb2.toString()+";");
-		}while(!temp.contains(";"));
+		}while(!temp.contains(";")); // ; 이 포함되어있으면 종료
 	}
 }
