0f8b7554e90402f8c73c1bcde5d4fd208e89d72b
Nokda <yh1483@naver.com>
2020-04-17 15:55:06 +0900

    2020-04-17 Problem 11053 added comment and reviewed.

--- 11000/Problem_11053/Main.java
+++ 11000/Problem_11053/Main.java
@@ -20,14 +20,14 @@ public class Main {
 		for(int i = 0 ; i < N; i++) {
 			int min = 0;
 			for(int j = 0; j<i; j++) {
-				if(arr[i] > arr[j]) {
-					if(min < dp[j]) {
+				if(arr[i] > arr[j]) { // 감소하는부분
+					if(min < dp[j]) { // dp[j]와 최소값을 비교
 						min = dp[j];
 					}
 				}
 			}
-			dp[i] = min + 1;
-			if(dp[i] > max) {
+			dp[i] = min + 1; // i까지의 가장 긴 증가하는 부분 수열 == 최소값 + 1
+			if(dp[i] > max) { // max하고 비교
 				max = dp[i];
 			}
 		}
