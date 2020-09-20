1084eec2faa54178caf3eb87354202815bed8d7c
Nokda <yh1483@naver.com>
2020-09-20 22:29:16 +0900

    2020-09-20 Problem 2011 added commented and reviewed.

--- 2000/Problem_2011/Main.java
+++ 2000/Problem_2011/Main.java
@@ -14,11 +14,11 @@ public class Main {
 		dp[1] = 1;
 
 		for (int i = 2; i <= str.length; i++) {
-			if (str[i - 1] - '0' > 0)
+			if (str[i - 1] - '0' > 0) // 이전 개수 그대로 가져옴
 				dp[i] = dp[i - 1];
 
-			int now = (str[i - 2] - '0') * 10 + (str[i - 1] - '0');
-			if (now >= 10 && now <= 26) {
+			int now = (str[i - 2] - '0') * 10 + (str[i - 1] - '0'); 
+			if (now >= 10 && now <= 26) { // 10~26번째 알파벳에 해당될때, 2단계 이전의 경우와 현재 개수와 합친다.
 				dp[i] = (dp[i] + dp[i - 2]) % 1000000;
 			}
 
