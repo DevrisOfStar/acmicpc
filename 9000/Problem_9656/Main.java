5b6d8303e0e87eeb9c14a78f82048b0956701654
Nokda <yh1483@naver.com>
2020-10-28 21:44:47 +0900

    2020-10-28 problem 9656 Solved.

---
+++ 9000/Problem_9656/Main.java
@@ -0,0 +1,21 @@
+package Problem_9656;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		boolean[] isWin = new boolean[N+3];
+		
+		isWin[1] = true; isWin[3] = true;
+					
+		for(int i = 4; i<=N; i++) // false, false일때 이김 
+			isWin[i] = !isWin[i-3] && !isWin[i-1];
+		
+		// 9655번과 반대
+		if(!isWin[N]) System.out.println("SK");
+		else System.out.println("CY");	
+	}
+}
