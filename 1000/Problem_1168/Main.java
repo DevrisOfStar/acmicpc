d8623cf27eddd29863147182d645cfbe111942c6
Nokda <yh1483@naver.com>
2020-11-21 00:37:02 +0900

    2020-11-12 Problem 1168 Solved.

--- 1000/Problem_1168/Main.java
+++ 1000/Problem_1168/Main.java
@@ -3,33 +3,28 @@ package Problem_1168;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
 		int N = Integer.parseInt(str.split(" ")[0]);
-		int K = Integer.parseInt(str.split(" ")[1]);
+		int K = Integer.parseInt(str.split(" ")[1])-1;
 		StringBuilder sb = new StringBuilder("<");
-		int[] arr = new int[N+1];
-		
-		for(int i = 0; i < N; i++) arr[i] = i+1;
-		
-		int i = K-1;
-		while(N > 0) {
-			if(N==1) {
-				sb.append(arr[0]).append(">\n");
-				break;
-			}
-			
-			sb.append(arr[i]).append(", ");
-			for(int j = i; j<N;j++) {
-				arr[j] = arr[j+1];
-			}
-			N--;
-			i = (i+K-1) % N;
+		ArrayList<Integer> arr = new ArrayList<>();
+		ArrayList<Integer> ans = new ArrayList<>();
+		for(int i = 0; i < N; i++) arr.add(i+1) ;
+		int idx =0;
+		for(int i = 0; i< N; i++) {
+			idx += K;
+			idx %= arr.size();
+			int tmp = arr.get(idx);
+			ans.add(tmp);
+			arr.remove(idx);
 		}
-
+		for(int i = 0; i<ans.size()-1;i++) sb.append(ans.get(i)).append(", ");
+		sb.append(ans.get(ans.size()-1)).append(">");
 		System.out.println(sb);
 
 	}
