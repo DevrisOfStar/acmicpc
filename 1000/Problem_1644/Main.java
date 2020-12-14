96b5ebc66c41d7075923bfc71324e70a68f623f9
Nokda <yh1483@naver.com>
2020-12-14 22:28:09 +0900

    2020-12-14 Problem 1644 Solved.

---
+++ 1000/Problem_1644/Main.java
@@ -0,0 +1,35 @@
+package Problem_1644;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int sq = (int)Math.sqrt(4000000);
+		boolean[] arr = new boolean[4000001];
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		for(int i = 2; i<= sq; i++) {
+			if(arr[i]) continue;
+			for(int j = i+i; j <= 4000000;j+=i) arr[j] = true;
+		}
+		
+		for(int i = 2; i<= 4000000;i++) if(!arr[i]) list.add(i);
+		int ans = 0;
+		int sum = 0;
+		int s = 0, e = 0;
+		while(e < list.size()) {
+			if(sum < N) {
+				sum += list.get(e++);
+				continue;
+			}
+			if(sum == N) ++ans;
+			sum -= list.get(s++);
+		}
+		
+		System.out.println(ans);
+		
+	}
+}
