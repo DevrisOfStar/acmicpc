f52f9d8a4df099ab5d869dcfd011ebda544ca8e8
Nokda <yh1483@naver.com>
2021-02-08 15:32:10 +0900

    2021-02-08 Problem 15353 Solved.

---
+++ 15000/Problem_15353/Main.java
@@ -0,0 +1,42 @@
+package Problem_15353;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	// 언어 제한 : C / C++
+	// Java로 문제만 풀었음, 채점하지 않음.
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String buf = br.readLine();
+		
+		StringBuilder a = new StringBuilder(buf.split(" ")[0]);
+		StringBuilder b = new StringBuilder(buf.split(" ")[1]);
+		a.reverse();
+		b.reverse();
+		String a_ = a.toString();
+		String b_ = b.toString();
+		StringBuilder c = new StringBuilder();
+		
+		boolean isTen = false;
+		
+		for(int i = 0; i<a_.length(); i++) {
+			int a__ = a_.charAt(i) - '0';
+			int b__ = b_.charAt(i) - '0';
+			
+			int c__ = a__ + b__;
+			if(isTen) {
+				c__ += 1;
+				isTen = false;
+			}
+			if(c__/10 == 1) isTen = true;
+			c__%=10;
+			
+			c.append(c__);
+		}
+		if(isTen) c.append(1);
+		System.out.println(c.reverse());
+	}
+}
