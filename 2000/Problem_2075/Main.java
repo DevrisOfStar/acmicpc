c7ef4d83763b2d740ca24468c3c6c275a03d91ba
Nokda <yh1483@naver.com>
2022-01-08 20:12:42 +0900

    2022-01-08 Problem 2075 Solved.

---
+++ 2000/Problem_2075/Main.java
@@ -0,0 +1,32 @@
+package Problem_2075;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int K = 0;
+		StringTokenizer st;
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2 - o1;
+			}
+		});
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				K = Integer.parseInt(st.nextToken());
+				pq.add(-K);
+				if (pq.size() > N) { pq.poll(); }
+			}
+		}
+		System.out.println(-pq.poll());
+	}
+}
