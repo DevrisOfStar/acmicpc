f7773f2a367b428986747cac54704d226d93cea5
Nokda <yh1483@naver.com>
2022-02-02 23:07:17 +0900

    2022-02-02 Problem 14629 Solved.

--- 14000/Problem_14629/Main.java
+++ 14000/Problem_14629/Main.java
@@ -6,12 +6,17 @@ public class Main {
 	static boolean[] isvisited = new boolean[10];
 	static long number;
 	static int size;
-	static long min = 1;
+	static long min;
 	static void answer(int idx, long num) {
 		if(idx <= 10) {
 			long cha = Math.abs(number - min);
-			long cha2 = Math.abs(num - min);
-			if(cha > cha2) min = num;
+			long cha2 = Math.abs(number - num);
+			if(cha > cha2) {
+				min = num;
+			}
+			else if(cha == cha2) {
+				if(min > num) min = num;
+			}
 		} 
 		
 		for(int i = 0; i<10;i++) {
@@ -28,6 +33,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		number = sc.nextLong();
 		size = String.valueOf(number).length();
+		min = number*2;
 		answer(0, 0);
 		System.out.println(min);
 	}
