5f8a96566b06ffd6d37051234527dce7598e9d0e
Nokda <yh1483@naver.com>
2022-02-11 17:48:49 +0900

    2022-02-11 Problem 5338 Solved.

---
+++ 5000/Problem_5338/Main.java
@@ -0,0 +1,13 @@
+package Problem_5338;
+
+public class Main {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("       _.-;;-._").append("\n");
+		sb.append("'-..-'|   ||   |").append("\n");
+		sb.append("'-..-'|_.-;;-._|").append("\n");
+		sb.append("'-..-'|   ||   |").append("\n");
+		sb.append("'-..-'|_.-''-._|").append("\n");
+		System.out.print(sb.toString());
+	}
+}
