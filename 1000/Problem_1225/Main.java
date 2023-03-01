e1e1fd3dfc102ca582773105208e7185b5909bf6
DevrisOfStar <yh1483@naver.com>
2023-03-01 23:37:15 +0900

    2023-03-01 Problem 1225 Solved.

--- 1000/Problem_1225/Main.java
+++ 1000/Problem_1225/Main.java
@@ -10,14 +10,11 @@ public class Main {
 		String str = br.readLine();
 		String a = str.split(" ")[0];
 		String b = str.split(" ")[1];
-		int[] arr_a = new int[10];
-		int[] arr_b = new int[10];
-		for(int i = 0; i<a.length(); i++) arr_a[a.charAt(i)-'0']++;
-		for(int i = 0; i<b.length(); i++) arr_b[b.charAt(i)-'0']++;
+		
 		long answer = 0;
-		for(int i = 0; i<10; i++) {
-			for(int j = 0; j<10; j++) {
-				answer += (i*j)*(arr_a[i]*arr_b[j]);
+		for(int i = 0; i<a.length(); i++) {
+			for(int j = 0; j<b.length(); j++) {
+				answer += ((a.charAt(i)-'0') * (b.charAt(j)-'0'));
 			}
 		}
 		System.out.println(answer);
